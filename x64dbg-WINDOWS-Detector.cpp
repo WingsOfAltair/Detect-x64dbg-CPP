109abca11035cc3cb84ddb61b02f34517f8b6e40
Bashar M. Bataineh <basharbataineh@hotmail.com>
2025-09-22 11:39:01 +0300

    Obfuscated string literals using XOR key
    Key should be changed per build distributed to ensure accountability.

--- x64dbg-WINDOWS-Detector.cpp
+++ x64dbg-WINDOWS-Detector.cpp
@@ -234,44 +234,62 @@ bool constant_time_equal_split(const std::wstring& input,
 bool lock_string(std::wstring& s)
 {
     if (s.empty()) return false;
-    void* p = &s[0];
-    SIZE_T bytes = s.size() * sizeof(wchar_t);
-    if (VirtualLock(p, bytes)) return true;
-    return false;
+    return VirtualLock(&s[0], s.size() * sizeof(wchar_t)) != 0;
 }
 
-void unlock_string(std::wstring& s)
+void unlock_and_zero_string(std::wstring& s)
 {
     if (s.empty()) return;
-    void* p = &s[0];
-    SIZE_T bytes = s.size() * sizeof(wchar_t);
-    VirtualUnlock(p, bytes);
+    SecureZeroMemory(&s[0], s.size() * sizeof(wchar_t));
+    VirtualUnlock(&s[0], s.size() * sizeof(wchar_t));
+    s.clear();
+    s.shrink_to_fit();
 }
 
-// securely zero an array of wide-strings (helper)
-void zero_and_free_parts(std::vector<std::wstring*>& parts)
-{
-    for (auto p : parts) {
-        if (!p) continue;
-        std::wstring& s = *p;
-        if (!s.empty()) {
-            lock_string(s);
-            SecureZeroMemory(&s[0], s.size() * sizeof(wchar_t));
-            s.clear();
-            s.shrink_to_fit();
-            unlock_string(s);
+class ObfuscatedSecret {
+private:
+    std::vector<wchar_t> encoded_;
+    wchar_t xorKey_;
+
+public:
+    ObfuscatedSecret(const std::wstring& secret, wchar_t key = 0xAA)
+        : xorKey_(key)
+    {
+        encoded_.reserve(secret.size());
+        for (auto c : secret)
+            encoded_.push_back(c ^ xorKey_);
+        if (!encoded_.empty())
+            VirtualLock(encoded_.data(), encoded_.size() * sizeof(wchar_t));
+    }
+
+    ~ObfuscatedSecret() { secure_zero(); }
+
+    bool check(const std::wstring& input) const
+    {
+        if (input.size() != encoded_.size()) return false;
+        volatile unsigned diff = 0;
+        for (size_t i = 0; i < input.size(); ++i)
+            diff |= (encoded_[i] ^ xorKey_) ^ input[i];
+        return diff == 0;
+    }
+
+    void secure_zero()
+    {
+        if (!encoded_.empty()) {
+            SecureZeroMemory(encoded_.data(), encoded_.size() * sizeof(wchar_t));
+            VirtualUnlock(encoded_.data(), encoded_.size() * sizeof(wchar_t));
+            encoded_.clear();
         }
     }
-}  
+};
 
-// print parts *without concatenating* (avoids temporaries containing full secret)
-void PrintPartsNoConcat(std::vector<std::wstring*>& parts)
+std::wstring decode_string(const wchar_t* obf, size_t len, wchar_t key)
 {
-    for (auto p : parts) {
-        if (!p) continue;
-        PrintWideNoNewLine(*p);
-    }
-    PrintWideNoNewLine(L"\n");
+    std::wstring out;
+    out.reserve(len);
+    for (size_t i = 0; i < len; ++i)
+        out.push_back(obf[i] ^ key);
+    return out;
 }
 
 // --------------------------- Main detector ---------------------------
@@ -282,153 +300,109 @@ int wmain()
 
     PrintWide(L"X64dbg Multi-layer debugger detection demo (with HW/SW breakpoints)\n");
 
-    bool anyDetected = false;
+    bool debuggerDetected = false;
 
-    // Basic checks
-    bool api_dbg = IsDebuggerPresent() != 0;
-    bool peb_dbg = check_peb_being_debugged();
-    bool veh_dbg = veh_breakpoint_test();
-    bool ntdll_hooked = check_nt_query_information_process_hooked();
-    bool dbgport = check_process_debug_port_via_nt();
-    bool hw_bp = any_thread_has_hw_breakpoints();
+    debuggerDetected |= (IsDebuggerPresent() != 0);
+    debuggerDetected |= check_peb_being_debugged();
+    debuggerDetected |= !veh_breakpoint_test();
+    debuggerDetected |= check_nt_query_information_process_hooked();
+    debuggerDetected |= check_process_debug_port_via_nt();
+    debuggerDetected |= any_thread_has_hw_breakpoints();
 
-    anyDetected |= api_dbg | peb_dbg | !veh_dbg | ntdll_hooked | dbgport | hw_bp;
-
-    // Software breakpoints
     for (void* addr : critical_addresses)
     {
         if (check_software_breakpoint(addr))
         {
-            anyDetected = true;
+            debuggerDetected = true;
         }
     }
 
-    std::wstring secretA = L"1";
-    std::wstring secretB = L"3";
-    std::wstring secretC = L"3";
-    std::wstring secretD = L"7";
+    constexpr wchar_t key = 0xAA;
 
-    std::wstring legitimateParts[] = {
-       L"L",L"e",L"g",L"i",L"t",L"i",L"m",L"a",L"t",L"e",L" ",L"C",L"o",L"p",L"y"
+    wchar_t secret_obf[] = { 0x31 ^ key, 0x33 ^ key, 0x33 ^ key, 0x37 ^ key };
+    wchar_t legit_obf[] = {
+        L'L' ^ key,L'e' ^ key,L'g' ^ key,L'i' ^ key,L't' ^ key,L'i' ^ key,L'm' ^ key,L'a' ^ key,L't' ^ key,L'e' ^ key,
+        L' ' ^ key,L'C' ^ key,L'o' ^ key,L'p' ^ key,L'y' ^ key
     };
-    std::wstring illegitimateParts[] = {
-        L"I",L"l",L"l",L"e",L"g",L"i",L"t",L"i",L"m",L"a",L"t",L"e",L" ",L"C",L"o",L"p",L"y"
+    wchar_t illegit_obf[] = {
+        L'I' ^ key,L'l' ^ key,L'l' ^ key,L'e' ^ key,L'g' ^ key,L'i' ^ key,L't' ^ key,L'i' ^ key,L'm' ^ key,L'a' ^ key,L't' ^ key,L'e' ^ key,
+        L' ' ^ key,L'C' ^ key,L'o' ^ key,L'p' ^ key,L'y' ^ key
     };
-    std::wstring enterSecretKeyParts[] = {
-        L"E",L"n",L"t",L"e",L"r",L" ",L"y",L"o",L"u",L"r",L" ",L"s",L"e",L"c",L"r",L"e",L"t", L" ", L"k", L"e", L"y", L":", L" "
+    wchar_t prompt_obf[] = {
+        L'E' ^ key,L'n' ^ key,L't' ^ key,L'e' ^ key,L'r' ^ key,L' ' ^ key,L'y' ^ key,L'o' ^ key,L'u' ^ key,L'r' ^ key,
+        L' ' ^ key,L's' ^ key,L'e' ^ key,L'c' ^ key,L'r' ^ key,L'e' ^ key,L't' ^ key,L' ' ^ key,L'k' ^ key,L'e' ^ key,L'y' ^ key,L':' ^ key,L' ' ^ key
     };
 
-    if (lock_string(secretA) == false) {
-        std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-        zero_and_free_parts(toZero);
+    std::wstring secret = decode_string(secret_obf, 4, key);
+    std::wstring legitMsg = decode_string(legit_obf, 15, key);
+    std::wstring illegitMsg = decode_string(illegit_obf, 17, key);
+    std::wstring promptMsg = decode_string(prompt_obf, 23, key);
 
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        ExitProcess(1);
-    }
-    if (lock_string(secretB) == false) {
-        std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-        zero_and_free_parts(toZero);
-
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        ExitProcess(1);
-    }
-    if (lock_string(secretC) == false) {
-        std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-        zero_and_free_parts(toZero);
-
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        ExitProcess(1);
-    }
-    if (lock_string(secretD) == false) {
-        std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-        zero_and_free_parts(toZero);
-
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        ExitProcess(1);
-    }
-
-    if (!anyDetected)
+    if (!lock_string(secret))
     {
-        std::wstring inputSecret;
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : enterSecretKeyParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        std::getline(std::wcin, inputSecret);
+        unlock_and_zero_string(secret);
+        unlock_and_zero_string(legitMsg);
+        unlock_and_zero_string(illegitMsg);
+        unlock_and_zero_string(promptMsg);
+        PrintWide(L"Failed to lock memory, exiting.\n");
+        ExitProcess(1);
+    }
 
-        bool api_dbg = IsDebuggerPresent() != 0;
-        bool peb_dbg = check_peb_being_debugged();
-        bool veh_dbg = veh_breakpoint_test();
-        bool ntdll_hooked = check_nt_query_information_process_hooked();
-        bool dbgport = check_process_debug_port_via_nt();
-        bool hw_bp = any_thread_has_hw_breakpoints();
+    if (debuggerDetected)
+    {
+        unlock_and_zero_string(secret);
+        unlock_and_zero_string(legitMsg);
+        PrintWide(illegitMsg);
+        unlock_and_zero_string(illegitMsg);
+        unlock_and_zero_string(promptMsg);
 
-        anyDetected |= api_dbg | peb_dbg | !veh_dbg | ntdll_hooked | dbgport | hw_bp;
+        ExitProcess(1);
+    }
 
-        if (anyDetected)
-        {
-            std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-            zero_and_free_parts(toZero);
+    PrintWide(promptMsg);
 
-            std::vector<std::wstring*> wordsToZero;
-            for (auto &s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-            PrintPartsNoConcat(wordsToZero);
-            zero_and_free_parts(wordsToZero);
-            ExitProcess(1);
-        }
+    std::wstring inputSecret;
+    std::getline(std::wcin, inputSecret);
 
-        if (constant_time_equal_split(inputSecret, secretA, secretB, secretC, secretD))
-        {
-            std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-            zero_and_free_parts(toZero);
+    // Recheck for debugger after input
+    debuggerDetected |= (IsDebuggerPresent() != 0);
+    debuggerDetected |= check_peb_being_debugged();
+    debuggerDetected |= !veh_breakpoint_test();
+    debuggerDetected |= check_nt_query_information_process_hooked();
+    debuggerDetected |= check_process_debug_port_via_nt();
+    debuggerDetected |= any_thread_has_hw_breakpoints();       
 
-            std::vector<std::wstring*> outParts;
-            for (auto& p : legitimateParts) outParts.push_back(&p);
-            PrintPartsNoConcat(outParts);
-            zero_and_free_parts(outParts);
-        }
-        else
-        {
-            std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-            zero_and_free_parts(toZero);
+    if (debuggerDetected)
+    {
+        unlock_and_zero_string(secret);
+        unlock_and_zero_string(legitMsg);
+        PrintWide(illegitMsg);
+        unlock_and_zero_string(illegitMsg);
+        unlock_and_zero_string(promptMsg);
 
-            std::vector<std::wstring*> wordsToZero;
-            for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-            PrintPartsNoConcat(wordsToZero);
-            zero_and_free_parts(wordsToZero);
-        }
+        ExitProcess(1);
+    }
+
+    if (constant_time_equal_split(inputSecret, secret.substr(0, 1), secret.substr(1, 1),
+        secret.substr(2, 1), secret.substr(3, 1)))
+    {
+        unlock_and_zero_string(secret);
+        PrintWide(legitMsg);
     }
     else
     {
-        std::vector<std::wstring*> toZero = { &secretA, &secretB, &secretC, &secretD };
-        zero_and_free_parts(toZero);
-
-        std::vector<std::wstring*> wordsToZero;
-        for (auto& s : illegitimateParts) wordsToZero.push_back(&const_cast<std::wstring&>(s));
-        PrintPartsNoConcat(wordsToZero);
-        zero_and_free_parts(wordsToZero);
-        ExitProcess(1);
+        unlock_and_zero_string(secret);
+        PrintWide(illegitMsg);
     }
 
-    std::vector<std::wstring*> toZero = { };
-    zero_and_free_parts(toZero);
+    // Zero messages before exit
+    unlock_and_zero_string(legitMsg);
+    unlock_and_zero_string(illegitMsg);
+    unlock_and_zero_string(promptMsg);
 
-    // Wait for a key using wide I/O
     PrintWide(L"Press Enter to exit...");
     std::wstring dummy;
     std::getline(std::wcin, dummy);
 
-    return anyDetected ? 1 : 0;
+    return debuggerDetected ? 1 : 0;
 }
